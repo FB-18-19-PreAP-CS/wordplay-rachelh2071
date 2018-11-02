395ca2559499eede69b42fad4743fb64702e588d
rachelh2071 <rachelh2071@gmail.com>
2018-11-02 13:39:14 -0500

    avoids works

--- textreader.py
+++ textreader.py
@@ -1,3 +1,4 @@
+
 #def read_file():
 #    
 #    with open("words.txt") as file: #file is name of variable
@@ -17,7 +18,19 @@ def at_least():
                 if len(word)>=20:  #line.strip() removes new line character
                     count_the +=1
         print(count_the)
-        
+def has_no_e(word):
+    '''
+>>> has_no_e('texas')
+False
+>>> has_no_e('longhorn')
+True
+>>> has_no_e('UTEP')
+False
+'''
+    if 'e' in word.lower():
+        return False
+    else:
+        return True
 def no_e():
     with open("words.txt") as file:
         linecount= 0
@@ -26,18 +39,48 @@ def no_e():
             linecount+=1
             if not 'e' in line:  
                 count +=1
-        print(f"%{(count/linecount)*100}")
+        print(f"{(count/linecount)*100:.3f}%")
         
 def avoids(word,str):
+    '''
+>>> avoids('Texas','tpr')
+False
+>>> avoids('Texas','lmn')
+True
+>>> avoids('Longhorn','LHN')
+False
+>>> avoids('truffle','dre')
+False
+'''
     for l in str:
         for i in range(len(word)):
-             if l == word[i]:
+             if l.lower() == word[i].lower():
                  return False
                 
     else:
         return True
-def count_avoids(word,str):
-    word = input("word:")
-    str = input("letters:")
+def count_avoids():
+    str = input('forbidden letters:')
+    with open("words.txt") as file: #file is name of variable
+        avoid = 0
+        for line in file:
+            for letter in line.strip().split():
+                if avoids(line,str):
+                    avoid+=1
+                    
+    print(avoid)
+def uses_only(word,str):
+    count = 0
+    for i in range(len(word)):
+        if word[i].lower() in str:
+            count+=1
+    if count == len(word):
+        return True
+    else:
+        return False
+def words_with_only(str):
+    pass
 if __name__=="__main__":
-    avoids("toll", 't')
+    import doctest
+    doctest.testmod()
+    count_avoids()
