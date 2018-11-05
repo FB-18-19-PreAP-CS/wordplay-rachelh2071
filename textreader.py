7a21cb1a27e95c1f35c6da0d1f10b545dfb4fb56
unknown <rachelh2071@gmail.com>
2018-11-04 21:33:15 -0600

    docstring and doctest add

--- textreader.py
+++ textreader.py
@@ -11,15 +11,18 @@
 #if __name__=="__main__":
 #    read_file()
 def at_least():
+    '''counts the number of words that have 20 characters or more'''
+    
     with open("words.txt") as file: #file is name of variable
-        count_the = 0
+        count = 0
         for line in file:
             for word in line.strip().split(): #line.split() splits line on spaces
                 if len(word)>=20:  #line.strip() removes new line character
-                    count_the +=1
+                    count +=1
         print(count_the)
 def has_no_e(word):
-    '''
+    '''returns True if the word doesn't contain the letter 'e' 
+
 >>> has_no_e('texas')
 False
 >>> has_no_e('longhorn')
@@ -32,6 +35,7 @@ False
     else:
         return True
 def no_e():
+    '''calculates the percentage of words that don't have the letter 'e'''
     with open("words.txt") as file:
         linecount= 0
         count = 0
@@ -42,7 +46,8 @@ def no_e():
         print(f"{(count/linecount)*100:.3f}%")
         
 def avoids(word,str):
-    '''
+    '''returns False if the word contains any letters from the string given
+
 >>> avoids('Texas','tpr')
 False
 >>> avoids('Texas','lmn')
@@ -55,11 +60,12 @@ False
     for l in str:
         for i in range(len(word)):
             if l.lower() == word[i].lower():
-                 return False
+                return False
                 
     else:
         return True
 def count_avoids():
+    '''counts the number of words that don't contain letters from the given string'''
     str = input('forbidden letters:')
     with open("words.txt") as file: #file is name of variable
         avoid = 0
@@ -70,6 +76,17 @@ def count_avoids():
                     
     print(avoid)
 def uses_only(word,str):
+    '''returns True if the word contains only letters from the given string
+
+>>> uses_only('rrrrrr','radical')
+True
+>>> uses_only('radical','rrrrr')
+False
+>>> uses_only('rad','radical')
+True
+
+
+'''
     count = 0
     for i in range(len(word)):
         if word[i].lower() in str:
@@ -79,11 +96,21 @@ def uses_only(word,str):
     else:
         return False
 def words_with_only(str):
+    '''prints all the words that only contain letters from the given string'''
     with open("words.txt") as file:
         for line in file:
             if uses_only(line.strip(),str):
                 print(line)
 def uses_all(word,str):
+    '''returns True if a word uses all of the letters from a given string at least once
+
+>>> uses_all('banannananan','anbar')
+False
+>>> uses_all('anbartsed', 'brtsdn')
+True
+>>> uses_all('definitavamente', 'dfeavmntiiiiii')
+True
+'''
     count = 0
     for i in range(len(str)):
         if str[i].lower() in word:
@@ -93,6 +120,7 @@ def uses_all(word,str):
     else:
         return False
 def how_many_uses_all(str):
+    '''counts how many words use all of the letters from a given string at least once'''
     count = 0
     with open("words.txt") as file:
         for line in file:
@@ -100,6 +128,15 @@ def how_many_uses_all(str):
                 count+=1
         print(count)
 def is_abecedarian(word):
+    '''returns True if the letters in a given word are in alphabetical order
+
+>>> is_abecedarian('abcdefg')
+True
+>>> is_abecedarian('abcdefe')
+False
+>>> is_abecedarian('abbbbbbbbbc')
+True
+'''
     count = 0
     for i in range(len(word)-1):
         if word[i] <= word[i+1]:
@@ -109,6 +146,7 @@ def is_abecedarian(word):
     else:
         return False
 def count_abecedarian():
+    '''count the number of words whose letters are in alphabetical order'''
     count = 0
     with open("words.txt") as file:
         for line in file:
@@ -118,5 +156,5 @@ def count_abecedarian():
 if __name__=="__main__":
     import doctest
     doctest.testmod()
-    count_abecedarian()
+
     
--- three_pair.py
+++ three_pair.py
@@ -1,6 +1,11 @@
-with open("words.txt") as file:
-    for line in file:
-        if len(line) >=6:
-            for i in range(len(line)-6):
-                if line[i] == line[i+1] and line[i+2] == line[i+3] and line[i+4]==line[i+5]:
-                    print(line)
+def three_pair():
+    '''finds the words that have 3 consecutive pairs of letters'''
+    with open("words.txt") as file:
+        for line in file:
+            if len(line) >=6:
+                for i in range(len(line)-6):
+                    if line[i] == line[i+1] and line[i+2] == line[i+3] and line[i+4]==line[i+5]:
+                        print(line)
+                        
+if __name__ == "__main__":
+    three_pair()
